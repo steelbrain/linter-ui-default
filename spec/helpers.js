e6356da8f9be1eb9b7ee1eef30ba743d231964e0
steelbrain <de.steelbrain@gmail.com>
2016-03-22 03:57:11 -0700

    :white_check_mark: Add specs for commands

--- spec/helpers.js
+++ spec/helpers.js
@@ -3,3 +3,39 @@
 export function getMessage(type = 'Error', filePath, range) {
   return { type, text: 'Some Message', filePath, range }
 }
+
+export function dispatchCommand(target: Object, commandName: string) {
+  atom.commands.dispatch(atom.views.getView(target), commandName)
+}
+
+// Our sugar method that allows us to pass async functions and do await in it
+export function it(name, callback) {
+  global.it(name, function() {
+    const value = callback()
+    if (value && value.constructor.name === 'Promise') {
+      waitsForPromise(function() {
+        return value
+      })
+    }
+  })
+}
+
+export function wait(timeout) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, timeout)
+  })
+}
+
+// Jasmine 1.3.x has no sane way of resetting to native clocks, and since we're
+// gonna test promises and such, we're gonna need it
+function resetClock() {
+  for (const key in jasmine.Clock.real) {
+    if (jasmine.Clock.real.hasOwnProperty(key)) {
+      window[key] = jasmine.Clock.real[key]
+    }
+  }
+}
+
+beforeEach(function() {
+  resetClock()
+})
