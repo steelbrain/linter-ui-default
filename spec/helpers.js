9514639b2fe5b4d788cd4c1e8f10183bfe119db4
steelbrain <de.steelbrain@gmail.com>
2016-07-16 11:58:30 -0700

    :white_check_mark: Add busy-signal specs

--- spec/helpers.js
+++ spec/helpers.js
@@ -4,6 +4,14 @@ export function getMessage(type = 'Error', filePath, range) {
   return { type, text: 'Some Message', filePath, range, version: 1 }
 }
 
+export function getLinter() {
+  return {
+    name: 'some',
+    grammarScopes: [],
+    lint() {},
+  }
+}
+
 export function dispatchCommand(target: Object, commandName: string) {
   atom.commands.dispatch(atom.views.getView(target), commandName)
 }
