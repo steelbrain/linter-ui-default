e3144fe8071d8a92850b01fb7eb13e76af53ee65
steelbrain <de.steelbrain@gmail.com>
2016-07-16 19:46:13 -0700

    :fire: Remove babel-eslint workaround

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'extends': 'steelbrain',
-  'rules': {
-    'no-shadow': 0, // Blocked on babel/babel-eslint#339
-    'no-param-reassign': 0,
-    'no-duplicate-imports': 0,
-    'no-underscore-dangle': 0
-  }
-}
