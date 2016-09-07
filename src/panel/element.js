86542d75ae2ea3376cfc1d059499f59ae6a39fe9
Steel Brain <i@steelbrain.me>
2016-09-07 05:30:16 -0700

    :new: Add React to eslint config

--- src/panel/element.js
+++ src/panel/element.js
@@ -1,9 +1,22 @@
 /* @flow */
 
 import React from 'react'
+import type Delegate from './delegate'
+import type { LinterMessage } from '../types'
 
 export default class PanelElement extends React.Component {
+  props: {
+    delegate: Delegate,
+  };
+  state: {
+    messages: Array<LinterMessage>,
+  } = { messages: [] };
+  componentDidMount() {
+    this.props.delegate.observeMessages(messages => {
+      this.setState({ messages })
+    })
+  }
   render() {
-    return <span></span>
+    return <span>Finding a React Table Component that fits our needs and is not bloatware is hard</span>
   }
 }
