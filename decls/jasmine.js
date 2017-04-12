463528059848f19420e370b577d47989f9df9aeb
Landon Abney <Landonabney@gmail.com>
2017-04-11 19:38:16 -0700

    Reorder the type declarations

--- decls/jasmine.js
+++ decls/jasmine.js
@@ -6,6 +6,6 @@ declare function fit(name: string, callback: (() => void)): void;
 declare function spyOn(object: Object, property: string): Object;
 declare function expect(value: any): Object;
 declare function describe(name: string, callback: (() => void)): void;
+declare function afterEach(callback: (() => void)): void;
 declare function fdescribe(name: string, callback: (() => void)): void;
 declare function beforeEach(callback: (() => void)): void;
-declare function afterEach(callback: (() => void)): void;
