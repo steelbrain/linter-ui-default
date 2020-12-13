430fa1074b78fd115ef8b26d916e67c1a9fb54e7
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-13 13:51:00 -0600

    fix: export functions directly in the index

--- lib/index.ts
+++ lib/index.ts
@@ -3,61 +3,58 @@ import type Intentions from './intentions'
 
 const idleCallbacks: Set<any> = new Set()
 
-type LinterUIDefault = {
-  instances: Set<LinterUI>
-  signalRegistry: Object | null
-  statusBarRegistry: Object | null
+let instances: Set<LinterUI> = new Set()
+let signalRegistry: Object | null = null
+let statusBarRegistry: Object | null = null
+
+export function activate() {
+  if (atom.config.get('linter-ui-default.useBusySignal')) {
+    // This is a necessary evil, see steelbrain/linter#1355
+    atom.packages.getLoadedPackage('linter-ui-default').metadata['package-deps'].push('busy-signal')
+  }
+
+  const callbackID = window.requestIdleCallback(function installLinterUIDefaultDeps() {
+    idleCallbacks.delete(callbackID)
+    if (!atom.inSpecMode()) {
+      // eslint-disable-next-line global-require
+      require('atom-package-deps').install('linter-ui-default')
+    }
+  })
+  idleCallbacks.add(callbackID)
 }
 
-const linterUiDefault = {
-  instances: new Set(),
-  signalRegistry: null,
-  statusBarRegistry: null,
-  activate() {
-    if (atom.config.get('linter-ui-default.useBusySignal')) {
-      // This is a necessary evil, see steelbrain/linter#1355
-      atom.packages.getLoadedPackage('linter-ui-default').metadata['package-deps'].push('busy-signal')
-    }
-
-    const callbackID = window.requestIdleCallback(function installLinterUIDefaultDeps() {
-      idleCallbacks.delete(callbackID)
-      if (!atom.inSpecMode()) {
-        // eslint-disable-next-line global-require
-        require('atom-package-deps').install('linter-ui-default')
-      }
-    })
-    idleCallbacks.add(callbackID)
-  },
-  deactivate() {
-    idleCallbacks.forEach(callbackID => window.cancelIdleCallback(callbackID))
-    idleCallbacks.clear()
-    for (const entry of this.instances) {
-      entry.dispose()
-    }
-    this.instances.clear()
-  },
-  provideUI(): LinterUI {
-    const instance = new LinterUI()
-    this.instances.add(instance)
-    if (this.signalRegistry) {
-      instance.signal.attach(this.signalRegistry)
-    }
-    return instance
-  },
-  provideIntentions(): Array<Intentions> {
-    return Array.from(this.instances).map(entry => entry.intentions)
-  },
-  consumeSignal(signalRegistry: Object) {
-    this.signalRegistry = signalRegistry
-    this.instances.forEach(function (instance) {
-      instance.signal.attach(signalRegistry)
-    })
-  },
-  consumeStatusBar(statusBarRegistry: Object) {
-    this.statusBarRegistry = statusBarRegistry
-    this.instances.forEach(function (instance) {
-      instance.statusBar.attach(statusBarRegistry)
-    })
-  },
+export function deactivate() {
+  idleCallbacks.forEach(callbackID => window.cancelIdleCallback(callbackID))
+  idleCallbacks.clear()
+  for (const entry of instances) {
+    entry.dispose()
+  }
+  instances.clear()
 }
-export default linterUiDefault
+
+export function provideUI(): LinterUI {
+  const instance = new LinterUI()
+  instances.add(instance)
+  if (signalRegistry) {
+    instance.signal.attach(signalRegistry)
+  }
+  return instance
+}
+
+export function provideIntentions(): Array<Intentions> {
+  return Array.from(instances).map(entry => entry.intentions)
+}
+
+export function consumeSignal(signalRegistry: Object) {
+  signalRegistry = signalRegistry
+  instances.forEach(function (instance) {
+    instance.signal.attach(signalRegistry)
+  })
+}
+
+export function consumeStatusBar(statusBarRegistry: Object) {
+  statusBarRegistry = statusBarRegistry
+  instances.forEach(function (instance) {
+    instance.statusBar.attach(statusBarRegistry)
+  })
+}
