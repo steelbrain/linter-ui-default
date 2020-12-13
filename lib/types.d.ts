675a15b936ba917cec6b9e99abbd3ae045ee33f7
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-13 14:04:59 -0600

    chore: add request callback types and make types .d.ts

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,5 +1,95 @@
+import { TextEditor, Point, Range } from 'atom'
+
+export type MessageSolution =
+  | {
+      title?: string
+      position: Range
+      priority?: number
+      currentText?: string
+      replaceWith: string
+    }
+  | {
+      title?: string
+      priority?: number
+      position: Range
+      apply: () => any
+    }
+
+export type Message = {
+  // Automatically added by linter
+  key: string
+  version: 2
+  linterName: string
+
+  // From providers
+  location: {
+    file: string
+    position: Range
+  }
+  reference?: {
+    file: string
+    position?: Point
+  }
+  url?: string
+  icon?: string
+  excerpt: string
+  severity: 'error' | 'warning' | 'info'
+  solutions?: Array<MessageSolution>
+  description?: string | (() => Promise<string> | string)
+}
+
+export type LinterMessage = Message
+
+export type Linter = {
+  // Automatically added
+  __$sb_linter_version: number
+  __$sb_linter_activated: boolean
+  __$sb_linter_request_latest: number
+  __$sb_linter_request_last_received: number
+
+  // From providers
+  name: string
+  scope: 'file' | 'project'
+  lintOnFly: boolean
+  grammarScopes: Array<string>
+  lint: (textEditor: TextEditor) => (Array<Message> | null | undefined) | Promise<Array<Message> | null | undefined>
+}
+
+export type MessagesPatch = {
+  added: Array<Message>
+  removed: Array<Message>
+  messages: Array<Message>
+}
+
+export type TreeViewHighlight = {
+  info: boolean
+  error: boolean
+  warning: boolean
+}
+
+// Electron types
 declare module 'electron' {
   export const shell: {
     openExternal(url: string): void
+  }
+}
+
+// windows requestIdleCallback types
+type RequestIdleCallbackHandle = any
+type RequestIdleCallbackOptions = {
+  timeout: number
+}
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean
+  timeRemaining: () => number
+}
+
+declare global {
+  interface Window {
+    requestIdleCallback: (
+      callback: (deadline: RequestIdleCallbackDeadline) => void,
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle
+    cancelIdleCallback: (handle: RequestIdleCallbackHandle) => void
   }
 }
