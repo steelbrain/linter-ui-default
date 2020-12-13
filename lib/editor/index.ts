c3ad076c9bbea446642e5811a885b95ef7e8631a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-13 17:45:27 -0600

    fix: use lineCount to detect if the editor is large

--- lib/editor/index.ts
+++ lib/editor/index.ts
@@ -225,7 +225,9 @@ export default class Editor {
 
           this.cursorPosition = getBufferPositionFromMouseEvent(event, this.textEditor, editorElement)
           this.ignoreTooltipInvocation = false
-          if (this.textEditor.largeFileMode) {
+          //@ts-ignore internal API
+          if (this.textEditor.largeFileMode || this.textEditor.getLineCount() > 20000) {
+            // TODO: make line count a config
             // NOTE: Ignore if file is too large
             this.cursorPosition = null
           }
