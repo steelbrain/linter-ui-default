9062d8130027137d984ac2d5a8e7e84851cd5c81
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-13 10:58:40 -0600

    chore: install @types/atom
    chore:

--- decls/atom.js
+++ decls/atom.js
@@ -6,20 +6,6 @@ declare var atom: Object;
 // Global Chromium CSS Object
 declare var CSS: Object;
 
-declare module 'atom' {
-  declare var Panel: any;
-  declare var Point: any;
-  declare var Range: any;
-  declare var Emitter: any;
-  declare var Disposable: any;
-  declare var TextEditor: any;
-  declare var TextBuffer: any;
-  declare var BufferMarker: any;
-  declare var TextEditorGutter: any;
-  declare var TextEditorMarker: any;
-  declare var CompositeDisposable: any;
-}
-
 declare module 'electron' {
   declare var shell: any;
 }
