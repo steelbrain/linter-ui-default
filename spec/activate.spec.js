e84d7800466cb5869ddd78b9fa77e85200514da6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-13 23:32:27 -0600

    test: add installation and activation tests

---
+++ spec/activate.spec.js
@@ -0,0 +1,35 @@
+describe("activation and installation", () => {
+  const deps = [
+    "intentions",
+    "linter"
+  ]
+
+  beforeEach(async () => {
+    jasmine.attachToDOM(atom.views.getView(atom.workspace))
+
+    /*    Activation     */
+    // Trigger deferred activation
+    atom.packages.triggerDeferredActivationHooks()
+    // Activate activation hook
+    atom.packages.triggerActivationHook("core:loaded-shell-environment")
+
+    // Activate the package
+    await atom.packages.activatePackage("linter-ui-default")
+  })
+
+  it("Installation", async function () {
+    expect(atom.packages.isPackageLoaded("linter-ui-default")).toBeTruthy()
+    const allDeps = atom.packages.getAvailablePackageNames()
+    deps.forEach((dep) => {
+      expect(allDeps.includes(dep)).toBeTruthy()
+    })
+  })
+
+  it("Activation", async function () {
+    expect(atom.packages.isPackageLoaded("linter-ui-default")).toBeTruthy()
+    deps.forEach(async (dep) => {
+      await atom.packages.activatePackage(dep)
+      expect(atom.packages.isPackageLoaded(dep)).toBeTruthy()
+    })
+  })
+})
