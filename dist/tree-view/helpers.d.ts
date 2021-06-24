afc7085eff5d15b075b4906050fbd0d81d872bc6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-24 04:47:03 -0500

    chore: add built files

--- dist/tree-view/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import type { LinterMessage } from '../types';
-import type { TreeViewHighlight } from './index';
-export declare function calculateDecorations(decorateOnTreeView: 'Files and Directories' | 'Files' | undefined, messages: Array<LinterMessage>): Record<string, TreeViewHighlight | undefined>;
